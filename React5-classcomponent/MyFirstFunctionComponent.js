6c61288495a3b443d5e81b7fc91f81974a1ed38d
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-10-26 16:58:22 +0530

    Add files via upload

---
+++ React5-classcomponent/MyFirstFunctionComponent.js
@@ -0,0 +1,12 @@
+
+
+function MyFirstFunctionComponent() {
+    return (
+      <div className="App">
+          <h1>MyFirstFunctionComponent..Ram</h1>
+      </div>
+    );
+  }
+  
+  export default MyFirstFunctionComponent;
+  
