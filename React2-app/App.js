afcba994acdabe756a8fdd9ebcde0d8f46b192fa
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-10-26 16:39:15 +0530

    Add files via upload

---
+++ React2-app/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const firstName="Ramesh";
+  const lastName="K";
+  const job="Techinical Software Eng";
+
+
+  return (
+    <div className="App">
+     <h1>Welcome to React JS Programming</h1> Hello
+      <h2>{firstName} </h2> 
+    
+    </div>
+  );
+}
+
+export default App;
