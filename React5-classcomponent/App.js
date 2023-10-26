0ca3f32b59a401f106608caf1640aac070031fb0
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-10-26 16:55:35 +0530

    Add files via upload

---
+++ React5-classcomponent/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MyFirstFunctionComponent from './MyFirstFunctionComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> This is App Component</h1>
+        <MyFirstFunctionComponent></MyFirstFunctionComponent>
+    </div>
+  );
+}
+
+export default App;
