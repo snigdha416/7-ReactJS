76e76cc0f1feb69ca3cf66f32063c789e4015bdf
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-11-22 21:45:01 +0530

    Add files via upload

---
+++ React14-useeffect/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import UseEffectHook1 from './UseEffectHook';
+
+function App() {
+  return (
+    <div className="App">
+      <UseEffectHook1></UseEffectHook1>
+    </div>
+  );
+}
+
+export default App;
