9c0f848fa92bad92dabc344f497a3a07c3a161f0
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-11-22 21:40:54 +0530

    Add files via upload

---
+++ React13-reacthooks/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import StateHook1 from './StateHook1';
+import StateHook2 from './StateHook2';
+
+
+function App() {
+  return (
+    <div className="App">
+        <StateHook2></StateHook2>
+    </div>
+
+
+
+  );
+}
+
+export default App;
