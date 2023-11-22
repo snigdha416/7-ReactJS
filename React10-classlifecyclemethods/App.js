661ce90f856590661e113984f689dc824125d732
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-11-22 21:28:24 +0530

    Add files via upload

---
+++ React10-classlifecyclemethods/App.js
@@ -0,0 +1,14 @@
+ 
+import './App.css';
+import MyContainer from './ClassLifeCycleMethods';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyContainer></MyContainer>
+    </div>
+  );
+}
+
+export default App;
