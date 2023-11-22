a91ea4d359563c0e73dc5d501e23ea4eae61168b
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-11-22 21:06:20 +0530

    Add files via upload

---
+++ React9-withstate/App.js
@@ -0,0 +1,12 @@
+import MyCounter from './MyCounter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyCounter></MyCounter>
+    </div>
+  );
+}
+
+export default App;
