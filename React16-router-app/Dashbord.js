52d24e670699b4a91050bb6b85b4d2c4028868da
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-11-22 21:54:09 +0530

    Add files via upload

---
+++ React16-router-app/Dashbord.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+
+class Dashboard extends Component{
+
+render(){
+    return(
+        <div>
+            <h1>Dashboard Page</h1>
+        </div>
+    );
+}
+
+}
+export default Dashboard
