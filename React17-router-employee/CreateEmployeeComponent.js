cfb5b50f57055e1ee832b4df8e116ac640db7a4b
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-11-22 22:29:01 +0530

    Add files via upload

---
+++ React17-router-employee/CreateEmployeeComponent.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react'
+class CreateEmployeeComponent extends Component {
+    
+
+    render() {
+        return (
+            <div>
+                <h1>CreateEmployeeComponent</h1>
+                <h2>CreateEmployeeComponent...</h2>
+            </div>
+        )
+    }
+}
+
+export default CreateEmployeeComponent
