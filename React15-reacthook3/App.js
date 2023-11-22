8089911d20bad2a48a127e2abdba715c983ba496
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-11-22 21:47:17 +0530

    Add files via upload

---
+++ React15-reacthook3/App.js
@@ -0,0 +1,16 @@
+import UseEffectHook1 from './UseEffectHook1';
+import UseEffectHook from './UseEffectHook';
+
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+         <UseEffectHook1/>
+    </div>
+  );
+}
+
+export default App;
