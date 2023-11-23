8b34a6098a5256dc8b1f748a62ec6dbf7e400b0b
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-11-23 08:56:30 +0530

    Add files via upload

---
+++ React17-router-employee/ListEmployeeComponent.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react'
+class ListEmployeeComponent extends Component {
+    
+
+    render() {
+        return (
+            <div>
+                <h1>ListEmployeeComponent</h1>
+                <h2>List All Employes...</h2>
+            </div>
+        )
+    }
+}
+
+export default ListEmployeeComponent
