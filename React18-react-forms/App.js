fd1c16b62a9440438fa08e1eb01f3eb727a386d5
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-11-23 09:12:20 +0530

    Add files via upload

---
+++ React18-react-forms/App.js
@@ -0,0 +1,17 @@
+import MyForm1 from './MyForm1';
+
+
+import MyForm2 from './MyForm2';
+import MyForm3 from './MyForm3'
+
+
+
+function App() {
+  return (
+    <div className="App">
+       <MyForm1></MyForm1>
+    </div>
+  );
+}
+
+export default App;
