80f817aeb9d24f5d55d8254f10b6fa8647f719b9
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-11-23 09:27:05 +0530

    Add files via upload

---
+++ React18-react-forms/MyForm1.js
@@ -0,0 +1,14 @@
+
+function MyForm1() {
+    return(
+      <form>
+         <label>Enter your Name :
+              <input type="text"/>
+
+         </label>
+      </form>
+
+    )
+}
+
+export default MyForm1;
