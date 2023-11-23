069e8ffcc8a18198ba34e35df8160f11d2ef6928
snigdha416 <113970805+snigdha416@users.noreply.github.com>
2023-11-23 08:57:58 +0530

    Add files via upload

---
+++ React17-router-employee/ViewEmployeeComponent.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react'
+class ViewEmployeeComponent extends Component {
+   
+
+    render() {
+        return (
+            <div>
+                <h1>ViewEmployeeComponent</h1>
+                <h2>ViewEmployeeComponent ...</h2>
+            </div>
+        )
+    }
+}
+
+export default ViewEmployeeComponent
